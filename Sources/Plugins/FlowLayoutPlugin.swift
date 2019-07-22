ec09650558aa4dd11120e1bd7bfe85ccd35efddd
Stefano Mondino <stefano.mondino.dev@gmail.com>
2019-07-22 21:15:09 +0200

    basic explanation of Flow algorithm

--- Sources/Plugins/FlowLayoutPlugin.swift
+++ Sources/Plugins/FlowLayoutPlugin.swift
@@ -34,6 +34,7 @@ extension PluginLayout {
 open class FlowLayoutPlugin: Plugin {
     public typealias Delegate = FlowLayoutDelegate
     public typealias Parameters = FlowSectionParameters
+    
     public var sectionHeadersPinToVisibleBounds: Bool = false
     public var sectionFootersPinToVisibleBounds: Bool = false
     
@@ -55,54 +56,94 @@ open class FlowLayoutPlugin: Plugin {
 
         let sectionParameters = self.sectionParameters(inSection: section, layout: layout)
         
+        //Create the header if available
         let header: UICollectionViewLayoutAttributes? = self.header(in: section, offset: &offset, layout: layout)
         
         let attributes: [UICollectionViewLayoutAttributes]
         let contentBounds = layout.contentBounds
         
+        //Accumulates attributes for last line.
         var lastLineAttributes: [UICollectionViewLayoutAttributes] = []
         
         if layout.scrollDirection == .vertical {
+            //Offset should be incremented by insets top, to create padding between header (if present) or previous section.
             offset.y += sectionParameters.insets.top
+            
+            //First line should start from current offset. It's the proper point (y) where the new attribute's origin should be placed
             var lineStart: CGFloat = offset.y
+            
+            //This is the highest point (y) reached by every attribute in current section
+            //At the beginning, since no attribute has been evaluated yet, this should be equal to lineStart
             var lineEnd = lineStart
+            
+            /*  Since layout is vertical, we need to "inflate" the offset horizontally.
+                This plugin handles line that have same width as encapsulating collection view, but other plugins previously evaluated could have inflated
+                the layout to higher values. For this reason, we are keeping that value if already bigger than bounds, otherwise we are inflating it.
+            */
             offset.x = max(offset.x, contentBounds.width)
+            
+            //Maximum width of each line. Should take into account section insets.
             let lineMaxWidth = contentBounds.width - sectionParameters.insets.left - sectionParameters.insets.right
+            
+            //Iterate through all items in current section
             attributes = (0..<collectionView.numberOfItems(inSection: section))
+                //convert each item into an IndexPath
                 .map { item in IndexPath(item: item, section: section) }
-                
+                //We use reduce to have access to last attribute's values
                 .reduce([]) { itemsAccumulator, indexPath -> [UICollectionViewLayoutAttributes] in
+                    //Create new attribute
                     let attribute = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+                    //Fetch attribute's size
                     let itemSize = self.itemSize(at: indexPath, collectionView: collectionView, layout: layout)
                     let origin: CGPoint
+                    //If a previous item exists (from second item in iteration on)
                     if let last = itemsAccumulator.last {
+                        //Desired starting x of freshly created item
                         let x = last.frame.maxX + sectionParameters.itemSpacing
+                        //If placing this item at desired x would cause item itself to overflow collection view bounds, create a new line
                         if x + itemSize.width + sectionParameters.insets.right > contentBounds.width {
+                            //Take all previous line attributes and re-space them
                             realignAttibutes(lastLineAttributes, inAvailableWidth: lineMaxWidth)
+                            //Recreate the line array with new attribute
                             lastLineAttributes = [attribute]
+                            //Place new attribute's origin on a new line.
                             origin = CGPoint(x: sectionParameters.insets.left, y: lineEnd + sectionParameters.lineSpacing)
+                            //Update the current lineStart value
                             lineStart = origin.y
                         } else {
+                            //We have space on current line, so we just update it with current attribute
                             lastLineAttributes += [attribute]
                             origin = CGPoint(x: x, y: lineStart)
                         }
                     } else {
+                        //This is the first item in this section
                         lastLineAttributes += [attribute]
+                        //Start from insets. Line start or end is the same in this case.
                         origin = CGPoint(x: sectionParameters.insets.left, y: lineEnd)
                     }
+                    //Finally set attribute's frame
                     attribute.frame = CGRect(origin: origin, size: itemSize)
+                    //If attribute is starting AFTER current line start
                     if attribute.frame.minY > lineStart {
+                        //Increase line start value to match the bottom-most item's origin
                         lineStart = attribute.frame.minY
                     }
+                    //If attribute is ending AFTER current line end
                     if attribute.frame.maxY > lineEnd {
+                        //Increase line end to match the bottom-most item's origin + height
                         lineEnd = attribute.frame.maxY
                     }
-                    
+                    //Append new item to the accumulator and proceed with iteration
                     return  itemsAccumulator + [attribute]
             }
+            
+            //Realign last line. Note: UICollectionViewFlowLayout seems to NOT do this.
             realignAttibutes(lastLineAttributes, inAvailableWidth: lineMaxWidth)
+            
+            //Update the offset with insets
             offset.y = lineEnd + sectionParameters.insets.bottom
         } else {
+            //Please refer to vertical explanation (this is just a copy of it, flipped for horizontal flow)
             offset.x += sectionParameters.insets.left
             var lineStart: CGFloat = offset.x
             var lineEnd = lineStart
@@ -144,8 +185,9 @@ open class FlowLayoutPlugin: Plugin {
             realignAttibutes(lastLineAttributes, inAvailableHeight: lineMaxHeight)
             offset.x = lineEnd + sectionParameters.insets.right
         }
-        
+        //Create a footer if possible
         let footer: UICollectionViewLayoutAttributes? = self.footer(in: section, offset: &offset, layout: layout)
+        //Return header + attributes + footer. If header or footer are not available (== nil), compactMap strips them away
         return ([header] + attributes + [footer]).compactMap { $0 }
         
     }
