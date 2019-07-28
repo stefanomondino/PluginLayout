aecb57c8db6afc4aa36c5fd95993cc550adb203f
Stefano Mondino <stefano.mondino.dev@gmail.com>
2019-07-28 19:54:17 +0200

    small adjustment to demo's header and footer

--- Example/DataSource/DataSource.swift
+++ Example/DataSource/DataSource.swift
@@ -10,11 +10,18 @@ import Foundation
 import UIKit
 
 class DataSource: NSObject, UICollectionViewDataSource {
-
-    var pictures: [[Picture]] = []
+    struct Section {
+        var pictures: [Picture]
+        var header: String?
+        var footer: String?
+    }
+    var sections: [Section] = []
+    
     init(pictures: [[Picture]]) {
         super.init()
-        self.pictures = pictures
+        sections = pictures.enumerated().map {
+            Section(pictures: $0.element, header: "Section #\($0.offset)", footer: "Total count: \($0.element.count)")
+        }
     }
     convenience init(pictures: [Picture]) {
         self.init(pictures: [pictures])
@@ -25,27 +32,33 @@ class DataSource: NSObject, UICollectionViewDataSource {
     }
     
     func picture(at indexPath: IndexPath) -> Picture {
-        return pictures[indexPath.section][indexPath.item]
+        return sections[indexPath.section].pictures[indexPath.item]
     }
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return pictures.count
+        return sections.count
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return pictures[section].count
+        return sections[section].pictures.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         collectionView.register(UINib(nibName: "PictureCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "picture")
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "picture", for: indexPath)
-        (cell as? PictureCollectionViewCell)?.picture = pictures[indexPath.section][indexPath.item]
+        (cell as? PictureCollectionViewCell)?.picture = sections[indexPath.section].pictures[indexPath.item]
         (cell as? PictureCollectionViewCell)?.number.text = "\(indexPath.item)"
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         collectionView.register(UINib(nibName: "SupplementaryCollectionViewCell", bundle: nil), forSupplementaryViewOfKind: kind, withReuseIdentifier: "supplementary")
         let cell = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "supplementary", for: indexPath)
-         (cell as? SupplementaryCollectionViewCell)?.titleLabel.text = "\(indexPath.section) " + kind.uppercased() 
+        let text: String
+        switch kind {
+        case UICollectionView.elementKindSectionHeader: text = sections[indexPath.section].header ?? ""
+        case UICollectionView.elementKindSectionFooter: text = sections[indexPath.section].footer ?? ""
+        default: text = "\(indexPath.section) " + kind.uppercased()
+        }
+         (cell as? SupplementaryCollectionViewCell)?.titleLabel.text = text
         return cell
     }
 }
